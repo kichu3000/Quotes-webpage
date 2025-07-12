9faff90a1ebeceada791799431e9aacaa251cc74
kichu3000 <anandakrishnanvb3000@gmail.com>
2025-07-12 21:25:32 +0530

    quotes app

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Randomquotes from './components/randomquotes'
+function App() {
+
+  return (
+    <div>
+        <Randomquotes/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/randomquotes.jsx
@@ -0,0 +1,88 @@
+import { useEffect, useState } from 'react';
+import './randomquotes.css'
+import { FaQuoteLeft , FaQuoteRight ,FaTwitter} from "react-icons/fa6";
+import axios from 'axios';
+function Randomquotes() {
+
+const [quotes,setQuotes] = useState({
+  'quote' : 'Do what you can, where you are, with what you have.',
+  'author' : 'Teddy Roosevelt'
+})
+const [color,setColor] = useState(`rgb(0,0,0)`)
+
+
+    const fetchData = async () =>{
+      const response = await axios.get('https://api.quotable.io/quotes/random');
+      console.log(response.data[0]);
+      setQuotes({
+        'quote' : response.data[0].content,
+        'author' : response.data[0].author
+      })
+        const randomColor = () =>{
+          const r = Math.floor(Math.random() * 160)
+          const g = Math.floor(Math.random() * 160)
+          const b = Math.floor(Math.random() * 156)
+          return `rgb(${r},${g},${b})`
+      }
+      console.log(randomColor());
+      const newcolor = randomColor();
+      setColor(newcolor);
+
+      // if (typeof document !== "undefined" && document.body) {
+      // document.body.style.backgroundColor = newcolor;
+      // }
+      document.body.style.backgroundColor = newcolor;
+
+      
+    }
+
+    
+
+    useEffect(() => {
+      fetchData();
+    },[])
+
+
+  return (
+    <div>
+      <div className="container">
+        <div className='quotes'>
+            <div style={{ fontWeight: "bold", fontSize: "1.2rem", display: "inline", alignItems: "flex-start", flexWrap: "wrap" }}>
+                <FaQuoteLeft
+                  size="32"
+                  style={{
+                    marginRight: "10px",color: "#000",flexShrink: 0, }}
+                />
+                <p className='heading' style={{ color: color }}>
+                  {quotes.quote}
+                </p>
+                <FaQuoteRight
+                  size="32"
+                  style={{ marginLeft: "10px", color: "#000", flexShrink: 0 }}
+                />
+            </div>
+            <h4  style={{ color: color }}>
+              - {quotes.author}
+            </h4>
+        </div>
+        <div className="buttons">
+            <a
+                href={`https://twitter.com/intent/tweet?text=${encodeURIComponent(` ${quotes.quote} -${quotes.author}`)}`}
+                className="twitter" 
+                style={{ backgroundColor: color }}
+                target="_blank"
+                rel="noopener noreferrer">
+                <FaTwitter/>
+              </a>
+            <button 
+              className="next-quote"
+              style= {{ backgroundColor: color }}
+              onClick={fetchData}
+            >Change Quote</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Randomquotes
